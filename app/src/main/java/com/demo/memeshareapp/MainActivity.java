fa51ea7621ed130565773810241bca44ac68a486
Keval Bhanderi <bhanderikeval007@gamil.com>
2023-08-25 18:40:58 +0530

    first commit

---
+++ app/src/main/java/com/demo/memeshareapp/MainActivity.java
@@ -0,0 +1,135 @@
+package com.demo.memeshareapp;
+
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ProgressBar pro;
+    Button button, button2;
+    ImageView imageView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        imageView2 = findViewById(R.id.imageView2);
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        pro = findViewById(R.id.pro);
+
+        loadmeme();
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, "Hey Guys, Checkout this cool meme i got from insta ");
+                Intent chooser = Intent.createChooser(intent, "Share thios amazing meme using.....");
+                startActivity(chooser);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadmeme();
+
+            }
+        });
+
+    }
+
+    private void loadmeme() {
+
+
+        pro.setVisibility(VISIBLE);
+
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = " https://meme-api.com/gimme";
+
+        // Request a string response from the provided URL.
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Display the first 500 characters of the response string.
+
+                        String url = null;
+                        try {
+                            url = response.getString("url");
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+
+
+                        Glide.with(MainActivity.this).load(url).listener(new RequestListener<Drawable>() {
+                            @Override
+                            public boolean onLoadFailed(@Nullable GlideException e, @Nullable Object model, @NonNull Target<Drawable> target, boolean isFirstResource) {
+
+                                pro.setVisibility(GONE);
+                                return false;
+                            }
+
+                            @Override
+                            public boolean onResourceReady(@NonNull Drawable resource, @NonNull Object model, Target<Drawable> target, @NonNull DataSource dataSource, boolean isFirstResource) {
+
+
+                                pro.setVisibility(GONE);
+                                return false;
+                            }
+                        }).into(imageView2);
+//                       Log.e("onResponse","Response is: " + response.substring(0, 500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.e("onErrorResponse", "That didn't work!");
+
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest);
+
+    }
+
+
+}
